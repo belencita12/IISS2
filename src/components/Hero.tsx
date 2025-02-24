49fdbff2816369a5ffa7136403dd7416ce4578d7
makiYamaGin <144964474+makiYamaGin@users.noreply.github.com>
2025-02-24 12:40:05 -0300

    cambiando color del boton

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,27 +2,31 @@ import Link from 'next/link'
 
 export default function Hero() {
     return (
-      <section className="flex items-center justify-center py-10 bg-white">
-        <div className="text-center mr-8">
-          <h2 className="text-3xl font-bold mb-4">¡Bienvenido a nuestra plataforma!</h2>
-          <p className="text-gray-600 mb-6">
-            Regístrate con nosotros y accede a una amplia gama de servicios para tu mascota.
-          </p>
-          <Link href="/register" >
-          <button className="bg-blue-600 text-white px-4 py-2 rounded-lg mr-4">Registrarse</button>
-          </Link>
-          <Link href="/login" >
-          <button className="bg-gray-300 px-4 py-2 rounded-lg">Iniciar Sesion</button>
-          </Link>
-        </div>
-        <img 
-          src="/image.png" 
-          alt="Logo1" 
-          width={300} 
-          height={300} 
-          className="object-contain"
-        />
-      </section>
+        <section className="flex items-center justify-center py-10 bg-white">
+            <div className="text-center mr-8">
+                <h2 className="text-3xl font-bold mb-4">¡Bienvenido a nuestra plataforma!</h2>
+                <p className="text-gray-600 mb-6">
+                    Regístrate con nosotros y accede a una amplia gama de servicios para tu mascota.
+                </p>
+                <Link href="/register" >
+                    <button className="bg-white text-black px-4 py-2 rounded-lg mr-4 border border-black">
+                        Registrarse
+                    </button>
+                </Link>
+                <Link href="/login" >
+                    <button className="bg-black  text-white px-4 py-2 rounded-lg">
+                        Iniciar Sesion
+                    </button>
+                </Link>
+            </div>
+            <img
+                src="/image.png"
+                alt="Logo1"
+                width={300}
+                height={300}
+                className="object-contain"
+            />
+        </section>
     );
-  }
+}
   
