9baa947b3adbdb3d61cf26d01d7d30a0d3e4fad6
makiYamaGin <144964474+makiYamaGin@users.noreply.github.com>
2025-02-25 10:33:50 -0300

    se borro pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
-import UnauthorizedPage from "@/pages/mainPage/unauthorizedPage";
+import Hero from "@/components/unauthorizePage/Hero";
+import Products from "@/components/unauthorizePage/Products";
+import Services from "@/components/unauthorizePage/Services";
 
 export default function Home() {
   return (
     <div>
-      <UnauthorizedPage />
+      <Hero />
+      <Services />
+      <Products />
+      <img
+        src="/image gato.png"
+        alt="Gato"
+        width={1500}
+        height={400}
+        className="object-contain rounded-xl"
+      />
     </div>
   );
 }
