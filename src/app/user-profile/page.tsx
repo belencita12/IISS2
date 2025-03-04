630f7ea2260f0f54821fa92d6f6b5b820dc87d3c
Gessica <adarogessica@gmail.com>
2025-03-03 23:18:55 -0300

    obtengo el nombre de usuario de la session

--- src/app/user-profile/page.tsx
+++ src/app/user-profile/page.tsx
@@ -2,11 +2,18 @@ import { Appointments } from "@/components/profile/Appointments";
 import { Header } from "@/components/profile/Header";
 import { PetsList } from "@/components/profile/PetLists";
 import { VeterinaryProducts } from "@/components/profile/Product";
+import { getServerSession } from "next-auth";
+import authOptions from "@/lib/auth/options"
 
-export default function Profile() {
+export default async function Profile() {
+    const session = await getServerSession(authOptions);
+    const fullName = session?.user?.fullName;
+
+    console.log("Sesión:", { fullName });
+
     return (
-        <div className="mx-auto">
-            <Header />
+        <div>
+            <Header fullName={fullName} />
             <PetsList />
             <Appointments/>
             <VeterinaryProducts />
--- src/components/profile/Header.tsx
+++ src/components/profile/Header.tsx
@@ -1,20 +1,25 @@
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
 
-export const Header = () => {
+interface HeaderProps {
+    fullName?: string;
+}
+
+export const Header = ({ fullName }: HeaderProps) => {
     return (
         <div className="bg-[#606060] p-6 flex items-center text-white justify-center">
             <Avatar className="w-20 h-20">
-                <AvatarImage  src="https://images.unsplash.com/photo-1517423440428-a5a00ad493e8" alt="Veterinario" />
+                <AvatarImage src="https://images.unsplash.com/photo-1517423440428-a5a00ad493e8" alt="Veterinario" />
             </Avatar>
             <div className="ml-4">
-                <h2 className="text-xl font-bold mt-2">Fulano Mengano</h2>
+                <h2 className="text-xl font-bold mt-2">{fullName}</h2>
                 <Badge className="bg-gray-300 text-black text-sm mt-2">Veterinaria Cliente Fiel</Badge>
                 <p className="text-base mt-2">Bienvenido a nuestro sistema de veterinaria</p>
             </div>
         </div>
     );
 };
+
 
 
 
