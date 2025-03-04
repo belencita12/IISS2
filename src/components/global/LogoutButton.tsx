8dadde8d683a3841d2c1cd93638c94b4434d90eb
Bryan Quintana <bryan.quintana@fiuni.edu.py>
2025-03-04 19:01:52 -0300

    Add session management to Navbar and implement LogoutButton

---
+++ src/components/global/LogoutButton.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { signOut } from "next-auth/react";
+import { useState } from "react";
+
+export default function LogoutButton() {
+  const [loading, setLoading] = useState(false);
+
+  const handleLogout = async () => {
+    setLoading(true);
+    await signOut({ callbackUrl: "/login" });
+  };
+
+  return (
+    <Button 
+      onClick={handleLogout}
+      variant="default"
+      className="hover:bg-gray-800"
+      disabled={loading}
+    >
+      {loading ? "Cerrando sesión..." : "Cerrar sesión"}
+    </Button>
+  );
+}
