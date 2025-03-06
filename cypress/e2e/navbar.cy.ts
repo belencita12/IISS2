37f8982b7cac7b26d5ea81ffc94c13577c78d0f7
LourdesVal <lourdesvalenzuela@fiuni.edu.py>
2025-03-06 15:28:30 -0300

    [208]-Test-Navbar

---
+++ cypress/e2e/navbar.cy.ts
@@ -0,0 +1,40 @@
+describe("Navbar", () => {
+    it("Navegar por los enlaces del navbar cuando no estás autenticado", () => {
+      cy.visit("/");
+      const links = [
+        { label: "Inicio", path: "/" },
+        { label: "Nosotros", path: "/nosotros" },
+        { label: "Servicios", path: "/servicios" },
+        { label: "Tienda", path: "/tienda" }
+      ];
+      
+      links.forEach(link => {
+        cy.contains(link.label).click();
+        cy.location('pathname').should('include', link.path);
+      });
+    });
+  
+    it("Navegar por los enlaces del navbar cuando estás autenticado y cerrar sesión", () => {
+      cy.visit("/login");
+      cy.get("input[name='email']").type("test1@gmail.com");
+      cy.get("input[name='password']").type("lourdes123");
+      cy.get("button[type='submit']").click();
+      cy.wait(3000);
+  
+      const links = [
+        { label: "Inicio", path: "/" },
+        { label: "Mi Perfil", path: "/user-profile" },
+        { label: "Nosotros", path: "/nosotros" },
+        { label: "Servicios", path: "/servicios" },
+        { label: "Tienda", path: "/tienda" }
+      ];
+  
+      links.forEach(link => {
+        cy.contains(link.label).click();
+        cy.location('pathname').should('include', link.path);
+      });
+  
+      cy.contains("Cerrar sesión").click();
+      cy.location('pathname').should('include', '/');
+    });
+  });
