62b40606b9a9fcdd44ebb31d087376e2784516a6
Gessica <adarogessica@gmail.com>
2025-03-06 09:57:03 -0300

    Manejo de mascota no existente

--- src/components/petUI/PetDetails.tsx
+++ src/components/petUI/PetDetails.tsx
@@ -51,15 +51,15 @@ interface Race {
 }
 
 interface Props {
-    token: string;
+  token: string;
 }
 
 const visitas: Visita[] = [
   { id: 1, fecha: "2024-12-20", descripcion: "Chequeo general", costo: 30000 },
-  { id: 2, fecha: "2024-10-15", descripcion: "Vacunación anual", costo: 50000},
-  { id: 3, fecha: "2024-09-10", descripcion: "Desparasitación", costo: 35000},
-  { id: 4, fecha: "2024-05-05", descripcion: "Consulta por tos", costo: 40000},
-  { id: 5, fecha: "2024-02-01", descripcion: "Revisión de piel", costo: 60000},
+  { id: 2, fecha: "2024-10-15", descripcion: "Vacunación anual", costo: 50000 },
+  { id: 3, fecha: "2024-09-10", descripcion: "Desparasitación", costo: 35000 },
+  { id: 4, fecha: "2024-05-05", descripcion: "Consulta por tos", costo: 40000 },
+  { id: 5, fecha: "2024-02-01", descripcion: "Revisión de piel", costo: 60000 },
 ]
 
 const visitasProgramadas: Visita_Programada[] = [
@@ -80,19 +80,23 @@ function calcularEdad(fechaNacimiento: string): string {
   const diaActual = hoy.getUTCDate();
 
   if (mesActual < mesNacimiento || (mesActual === mesNacimiento && diaActual < diaNacimiento)) {
-      edad--;
+    edad--;
   }
 
   if (edad < 1) {
-    if(agediff > 0){ meses = 12 + mesActual - mesNacimiento;
-    } else { meses = mesActual - mesNacimiento;
+    if (agediff > 0) {
+      meses = 12 + mesActual - mesNacimiento;
+    } else {
+      meses = mesActual - mesNacimiento;
     }
-    if(meses === 1) { return `${meses} Mes`
-    } else { return `${meses} Meses`
+    if (meses === 1) {
+      return `${meses} Mes`
+    } else {
+      return `${meses} Meses`
     }
   }
 
-  if (edad === 1){
+  if (edad === 1) {
     return `${edad} Año`;
   }
   return `${edad} Años`;
@@ -101,10 +105,10 @@ function calcularEdad(fechaNacimiento: string): string {
 function convertirFecha(fecha: string): string {
   console.log(fecha);
   const meses: string[] = [
-      "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
-      "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
+    "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+    "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
   ];
-  
+
   const fechaObj = new Date(fecha);
   const dia = fechaObj.getUTCDate();
   const mes = meses[fechaObj.getUTCMonth()];
@@ -117,13 +121,13 @@ function formatNumber(num: number) {
   return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 }
 
-export default function PetDetails({token}: Props) {
-  const {id} = useParams();
+export default function PetDetails({ token }: Props) {
+  const { id } = useParams();
 
   const [showAll, setShowAll] = useState(false);
   const [showAllProg, setShowAllProg] = useState(false);
   const [data, setData] = useState(null);
-  const [pet, setPet] = useState<Pet | null>(null);
+  const [pet, setPet] = useState<Pet | null | undefined>(undefined);
   const [isEditing, setIsEditing] = useState(false);
   const [isEditingName, setIsEditingName] = useState(false);
 
@@ -174,7 +178,7 @@ export default function PetDetails({token}: Props) {
   }, [pet])
 
   useEffect(() => {
-    if(isEditing && pet) {
+    if (isEditing && pet) {
       setEditedPet({
         name: pet.name || "",
         dateOfBirth: pet.dateOfBirth || "",
@@ -185,8 +189,9 @@ export default function PetDetails({token}: Props) {
       });
     }
   }, [isEditing, pet]);
-  
+
   useEffect(() => {
+    setPet(undefined);
     fetch(`https://iiss2-backend-production.up.railway.app/pet/${id}`, {
       method: "GET",
       headers: {
@@ -196,18 +201,24 @@ export default function PetDetails({token}: Props) {
     })
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
-        setPet(data);
+        if (data && data.id) {
+          setPet(data);
+        } else {
+          setPet(null);
+        }
       })
-      .catch((error) => console.error("Error:", error));
-  }, []);
+      .catch((error) => {
+        console.error("Error:", error);
+        setPet(null);
+      });
+  }, [id, token]);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-    const {name, value} = e.target;
+    const { name, value } = e.target;
 
     setEditedPet((prev) => {
       if (!prev) return prev;
-      return{
+      return {
         ...prev,
         [name]: value,
       }
@@ -216,14 +227,14 @@ export default function PetDetails({token}: Props) {
 
 
   const handleSave = async () => {
-    if(!editedPet || !pet) {
+    if (!editedPet || !pet) {
       console.error("Error: EditedPet o pet es null");
       return;
     }
 
     try {
       const updatedPet = {
-        name: editedPet.name,   
+        name: editedPet.name,
       }
 
       const response = await fetch(`https://iiss2-backend-production.up.railway.app/pet/${pet?.id}`, {
@@ -247,20 +258,25 @@ export default function PetDetails({token}: Props) {
       console.error("Error al actualizar los datos de la mascota", error);
     }
   };
-  
+
 
   return (
     <div className="flex-col">
-      {pet ? (
+      {pet === undefined ? (
+        <p className="text-center text-gray-600">Cargando mascota...</p>
+      ) : pet === null ? (
+        <p className="text-center mt-4 p-10">Mascota no registrada.</p>
+      ) : (
+        <>
         <div className="flex justify-center bg-gray-500 p-5">
           <div className=" flex-col justify-center items-center p-3 pr-8">
-            <div className="w-[250px] h-[250px] rounded-full overflow-hidden border-[3px] border-black flex justify-center items-center">              
+            <div className="w-[250px] h-[250px] rounded-full overflow-hidden border-[3px] border-black flex justify-center items-center">
               {pet.profileImg ? (
                 <Image
                   src={pet.profileImg}
                   alt={pet.name}
                   width={100}
-                  height={100}                
+                  height={100}
                   className="object-cover object-center w-full h-full"
                 />
               ) : (
@@ -373,12 +389,12 @@ export default function PetDetails({token}: Props) {
                     {name === "dateOfBirth"
                       ? convertirFecha(pet.dateOfBirth)
                       : name === "race"
-                      ? pet.race?.name
-                      : name === "species"
-                      ? pet.species?.name
-                      : name === "weight"
-                      ? `${pet.weight} kg`
-                      : String(pet[name as keyof Pet])}
+                        ? pet.race?.name
+                        : name === "species"
+                          ? pet.species?.name
+                          : name === "weight"
+                            ? `${pet.weight} kg`
+                            : String(pet[name as keyof Pet])}
                   </p>
                 )}
               </div>
@@ -395,60 +411,60 @@ export default function PetDetails({token}: Props) {
 
 
         </div>
-      ) : (<p className="text-center text-gray-600">Cargando mascota...</p>)}
+        <div className="flex-col p-7 bg-white">
+          <div className="flex justify-center">
+            <div className="flex-col justify-center items-center w-[80%] pb-7 pt-7">
+              <h2 className="text-2xl font-bold mb-3">Últimas visitas</h2>
+              <ul className="w-full">
+                {visitasVisibles.map((visita) => (
+                  <li key={visita.id} className="mb-2 border border-gray-400 p-2 rounded">
+                    <div className="flex justify-between">
+                      <p className="text-base">{visita.descripcion}</p>
+                      <p className="text-sm text-gray-600">{convertirFecha(visita.fecha)}</p>
+                    </div>
+                    <p className="text-base text-left">{formatNumber(visita.costo)} Gs</p>
+                  </li>
+                ))}
+              </ul>
+              {visitas.length > 4 && (
+                <div className="flex justify-center">
+                  <button
+                    onClick={() => setShowAll(!showAll)}
+                    className="mt-3 bg-gray-400 text-white rounded-lg p-2 pr-5 pl-5">
+                    {showAll ? "Ver menos" : "Ver más"}
+                  </button>
+                </div>
+              )}
+            </div>
+          </div>
 
-      <div className="flex-col p-7 bg-white">
-        <div className="flex justify-center">
-          <div className="flex-col justify-center items-center w-[80%] pb-7 pt-7">
-            <h2 className="text-2xl font-bold mb-3">Últimas visitas</h2>
-            <ul className="w-full">
-              {visitasVisibles.map((visita) => (
-                <li key={visita.id} className="mb-2 border border-gray-400 p-2 rounded">
-                  <div className="flex justify-between">
-                    <p className="text-base">{visita.descripcion}</p>
-                    <p className="text-sm text-gray-600">{convertirFecha(visita.fecha)}</p>                    
-                  </div>
-                  <p className="text-base text-left">{formatNumber(visita.costo)} Gs</p>
-                </li>
-              ))}
-            </ul>
-            {visitas.length > 4 && (
-              <div className="flex justify-center">
-                <button
-                  onClick={() => setShowAll(!showAll)}
-                  className="mt-3 bg-gray-400 text-white rounded-lg p-2 pr-5 pl-5">
-                  {showAll ? "Ver menos" : "Ver más"}
-                </button>
-              </div>
-            )}
+          <div className="flex justify-center">
+            <div className="flex-col justify-center items-center w-[70%] pb-7 pt-7">
+              <h2 className="text-2xl font-bold mb-3">Visitas programadas</h2>
+              <ul className="w-full grid grid-cols-2 gap-4">
+
+                {visitasProgramadasVisibles.map((visita) => (
+                  <li key={visita.id} className="border border-gray-400 p-2 rounded flex">
+                    <AlertCircle className="w-5 h-5" />
+                    <div className="ml-3">
+                      <p className="text-base">{visita.descripcion}</p>
+                      <p className="text-sm text-gray-600">{convertirFecha(visita.fecha)}</p>
+                      <Button>Añadir Recordatorio</Button>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+              {visitasProgramadas.length > 4 && (
+                <Button onClick={() => setShowAllProg(!showAllProg)}
+                  className="mt-3 text-blue-500 hover:underline">
+                  {showAllProg ? "Ver menos" : "Ver más"}
+                </Button>
+              )}
+            </div>
           </div>
         </div>
-
-        <div className="flex justify-center">
-          <div className="flex-col justify-center items-center w-[70%] pb-7 pt-7">
-            <h2 className="text-2xl font-bold mb-3">Visitas programadas</h2>
-            <ul className="w-full grid grid-cols-2 gap-4">
-              
-              {visitasProgramadasVisibles.map((visita) => (
-                <li key={visita.id} className="border border-gray-400 p-2 rounded flex">
-                  <AlertCircle className="w-5 h-5" />
-                  <div className="ml-3">
-                    <p className="text-base">{visita.descripcion}</p>
-                    <p className="text-sm text-gray-600">{convertirFecha(visita.fecha)}</p>
-                    <Button>Añadir Recordatorio</Button>  
-                  </div>            
-                </li>
-              ))}
-            </ul>
-            {visitasProgramadas.length > 4 && (
-              <Button onClick={() => setShowAllProg(!showAllProg)}
-                className="mt-3 text-blue-500 hover:underline">
-                {showAllProg ? "Ver menos" : "Ver más"}
-              </Button>
-            )}
-          </div>
-        </div>
-      </div>
+      </>
+    )}
     </div>
   );
 }
