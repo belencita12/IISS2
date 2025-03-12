bdd6f32a708c0da0caf3903994bebd5d8257967c
makiYamaGin <144964474+makiYamaGin@users.noreply.github.com>
2025-03-12 20:23:20 -0300

    task 212 instalado chart.js
    se instalo  pnpm install react-chartjs-2 chart.js para mostrar las graficas, navlinks que me pidio, cambiado. me falta ver responsive

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,12 +1,23 @@
 import SideNav from "@/components/global/SideBar";
+import type { Metadata, Viewport } from "next";
+
+export const metadata: Metadata = {
+  title: "NicoPets",
+  description: "Servicios y productos para tus mascotas",
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1.0,
+};
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+    <div className="flex h-auto flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64 md:sticky">
         <SideNav />
       </div>
-      <div className="flex-grow py-6 px-3 md:overflow-y-auto md:p-12">
+      <div className="flex-grow md:overflow-y-auto ">
         {children}
       </div>
     </div>
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,52 @@
+import { Header } from '@/components/admin/Header'
+import { LineChart, PieChart, BarChart } from '@/components/admin/Charts';
+import { DollarSign, Users } from 'lucide-react';
+
 export default function Dashboard() {
-  return <></>;
+
+
+  return (
+    <div>
+      <Header fullName="Jorge Oviedo" />
+      <div className="p-8 space-y-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6 mb-6">
+          {/* Total Facturado */}
+        <div className="bg-white p-4 shadow rounded-lg flex items-center justify-between">
+          <div>
+            <h3 className="text-sm text-gray-500">Total Facturado</h3>
+            <p className="text-2xl font-bold">$10,000</p>
+          </div>
+          <DollarSign size={32} className="text-green-500" />
+        </div>
+
+        {/* Clientes */}
+        <div className="bg-white p-4 shadow rounded-lg flex items-center justify-between">
+          <div>
+            <h3 className="text-sm text-gray-500">Clientes</h3>
+            <p className="text-2xl font-bold">150</p>
+          </div>
+          <Users size={32} className="text-blue-500" />
+        </div>
+        </div>
+        
+
+
+        <div className="grid grid-cols-1 md:grid-cols-2  gap-6">
+          <div className="w-full h-80">
+            <LineChart />
+          </div>
+          <div className="w-full h-60">
+            <PieChart />
+          </div>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2  gap-6">
+          <div className="w-full h-96 mt-6">
+            <BarChart />
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  );
 }
