324ab035b606bf468cb23fe9d17ebd96a0bf828b
LourdesVal <lourdesvalenzuela@fiuni.edu.py>
2025-03-11 21:35:13 -0300

    [236]-header-admin

--- src/components/admin/Header.tsx
+++ src/components/admin/Header.tsx
@@ -1,18 +1,14 @@
-import { Avatar } from "@/components/ui/avatar";
-import { Globe } from "lucide-react"; 
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
 
 interface HeaderProps {
-    fullName?: string;
+    fullName: string;
 }
 
 export const Header = ({ fullName }: HeaderProps) => {
     return (
         <div className="bg-[#606060] p-6 flex items-center text-white w-full">
-            <Avatar className="w-20 h-20 flex items-center justify-center rounded-full bg-white">
-                <Globe className="w-20 h-20 text-black" />
-                <div className="absolute top-1/2 left-0 right-0 transform -translate-y-1/2 bg-yellow-400 text-black font-bold py-1 text-xs text-center">
-                        ADMIN
-                </div>
+            <Avatar className="w-20 h-20 ml-20">
+                <AvatarImage src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png" alt="Usuario" />
             </Avatar>
             <div className="ml-4 flex-1">
                 <h2 className="text-xl font-bold mt-2">{fullName}</h2>
