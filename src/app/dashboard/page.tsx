fb4a816a1f0f75d03e06fdb984859d782cd3cc8b
LourdesVal <lourdesvalenzuela@fiuni.edu.py>
2025-03-11 21:41:38 -0300

    [236]-header-admin

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,18 +1,3 @@
-import { Header } from "@/components/admin/Header";
-import { getServerSession } from "next-auth";
-import authOptions from "@/lib/auth/options";
-import { redirect } from "next/navigation";
-
-export default async function Dashboard() {
-   const session = await getServerSession(authOptions);
-    console.log("Sesión:", session?.user?.fullName);
-    console.log("Sesión:", session?.user?.token);
-    if (!session) {
-        redirect("/login");
-    }
-    return (
-        <div>
-            <Header fullName={session?.user?.fullName} />
-        </div>
-    );
+export default function Dashboard() {
+  return <></>;
 }
