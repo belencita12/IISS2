c1b0f4439dce90d4a71854086bc072145cadc8c4
AnniaBenitez <anniamicaela@gmail.com>
2025-03-18 23:24:28 -0300

    arreglado el tema de id para build

--- src/app/dashboard/vaccine/edit/[id]/page.tsx
+++ src/app/dashboard/vaccine/edit/[id]/page.tsx
@@ -1,18 +1,22 @@
+// app/dashboard/vaccine/edit/[id]/page.tsx
 import EditVaccinePage from "@/components/admin/vaccine/EditVaccinePage";
 import authOptions from "@/lib/auth/options";
 import { getServerSession } from "next-auth";
 import { notFound } from "next/navigation";
 
-export default async function Page(context: { params: { id: string } }) {
-  const params = await Promise.resolve(context.params);
-  const id = params.id;
-
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params; // Esperamos la resolución de params (similar a await params en el ejemplo de slug)
+  
   const session = await getServerSession(authOptions);
   const token = session?.user?.token || null;
-
+  
   if (!token) {
     notFound();
   }
-
+  
   return <EditVaccinePage token={token} id={id} />;
 }
