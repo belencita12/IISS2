9d9a516dd87936d07c9ba12d6480a5241fec9a05
AnniaBenitez <anniamicaela@gmail.com>
2025-03-19 11:16:23 -0300

    cambio de url por variable url

--- src/lib/vaccine-manufacturer/getVaccineManufacturerById.ts
+++ src/lib/vaccine-manufacturer/getVaccineManufacturerById.ts
@@ -1,7 +1,7 @@
-const API_URL = `${process.env.NEXT_PUBLIC_BASE_URL}/vaccine-manufacturer`;
+import { VACCINE_MANUFACTURER_API } from "@/lib/urls";
 
 export async function createManufacturer(token: string, data: { name: string }) {
-  const response = await fetch(API_URL, {
+  const response = await fetch(VACCINE_MANUFACTURER_API, {
     method: 'POST',
     headers: { 
       'Content-Type': 'application/json',
@@ -15,7 +15,7 @@ export async function createManufacturer(token: string, data: { name: string }) 
 }
 
 export async function updateManufacturer(token: string, id: number, data: { name: string }) {
-  const response = await fetch(`${API_URL}/${id}`, {
+  const response = await fetch(`${VACCINE_MANUFACTURER_API}/${id}`, {
     method: 'PATCH',
     headers: { 
       'Content-Type': 'application/json',
@@ -29,7 +29,7 @@ export async function updateManufacturer(token: string, id: number, data: { name
 }
 
 export async function getManufacturers(token: string, page: number = 1, searchQuery: string = '') {
-  const url = new URL(API_URL);
+  const url = new URL(VACCINE_MANUFACTURER_API);
   url.searchParams.append("page", page.toString());
   if (searchQuery) {
     url.searchParams.append("name", searchQuery);
@@ -47,7 +47,7 @@ export async function getManufacturers(token: string, page: number = 1, searchQu
 }
 
 export async function getManufacturerById(token: string, id: number) {
-  const response = await fetch(`${API_URL}/${id}`, {
+  const response = await fetch(`${VACCINE_MANUFACTURER_API}/${id}`, {
     method: 'GET',
     headers: {
       'Authorization': `Bearer ${token}`
@@ -59,7 +59,7 @@ export async function getManufacturerById(token: string, id: number) {
 }
 
 export async function deleteManufacturer(token: string, id: number) {
-  const response = await fetch(`${API_URL}/${id}`, {
+  const response = await fetch(`${VACCINE_MANUFACTURER_API}/${id}`, {
     method: 'DELETE',
     headers: {
       'Authorization': `Bearer ${token}`
