286d8d4fb90ae3667074d0c7906ade2e99183838
SAZAUNI <santiago.zarza@fiuni.edu.py>
2025-03-19 12:19:41 -0300

    Creado: pagina para editar mascota (ADMIN)

--- src/app/detalle-mascota/[id]/page.tsx
+++ src/app/detalle-mascota/[id]/page.tsx
@@ -1,17 +1,25 @@
+import AdminPetDetails from "@/components/admin/pet/AdminPetDetails";
 import PetDetails from "@/components/petUI/PetDetails";
 import authOptions from "@/lib/auth/options";
 import { getServerSession } from "next-auth/next";
 
-
 export default async function Page() {
   const session = await getServerSession(authOptions);
-  if(session){
+  if (session) {
     const token = session?.user.token;
+    const roles = session?.user.roles;
+
     console.log(token);
 
-    return <div>
-      <PetDetails token={token} />
-    </div>
+    return (
+      <div>
+        {roles.includes("ADMIN") ? (
+          <AdminPetDetails token={token} />
+        ) : (
+          <PetDetails token={token} />
+        )}
+      </div>
+    );
   }
   return;
 }
