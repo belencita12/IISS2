d63966f62130b1714aac530a292f34033826800b
Bryan Quintana <bryan.quintana@fiuni.edu.py>
2025-03-23 15:52:52 -0300

    refactor: update import paths for PetDetails and PetList components for consistency

--- src/app/user-profile/pet/list-pets/page.tsx
+++ src/app/user-profile/pet/list-pets/page.tsx
@@ -1,4 +1,4 @@
-import PetList from '@/components/petUI/PetList'
+import PetList from '@/components/pet/PetList'
 import authOptions from '@/lib/auth/options'
 import { getServerSession } from 'next-auth'
 import { redirect } from 'next/navigation'
