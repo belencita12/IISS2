fedd083abc8ce6b33fc424214202e5016ba75b1d
Agus3160 <ampharos321@hotmail.com>
2025-03-28 15:24:02 -0300

    [268] useQuery hook + regExp de validaciones varias

---
+++ src/hooks/useQuery.tsx
@@ -0,0 +1,19 @@
+import { BaseQueryParams } from "@/lib/types";
+import { useCallback, useState } from "react";
+
+export const useQuery = <T extends BaseQueryParams>(init?: T) => {
+  const [query, setQuery] = useState<T>(init || ({ page: 1 } as T));
+
+  const toQueryString = useCallback(() => {
+    const url = new URLSearchParams();
+    Object.entries(query).forEach(([key, value]) => {
+      if (value) {
+        if (value instanceof Date) url.set(key, value.toISOString());
+        url.set(key, String(value));
+      }
+    });
+    return url.toString();
+  }, [query]);
+
+  return { query, setQuery, toQueryString };
+};
