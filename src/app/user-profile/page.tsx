a9073cc8bab5f025d4e96a5e94f148c2d270c09d
Matias Brunaga <genaro.brunaga@fiuni.edu.py>
2025-04-07 01:35:48 -0300

    correccion del bug. se creo getAuth se hace uso de getAuth en Header para obtener los datos del usuario en sesion. se modifico user-profile/page para que pase el token a Header.

--- src/app/user-profile/page.tsx
+++ src/app/user-profile/page.tsx
@@ -4,29 +4,23 @@ import { PetsList } from "@/components/profile/PetLists";
 import { VeterinaryProducts } from "@/components/profile/Product";
 import { getServerSession } from "next-auth";
 import authOptions from "@/lib/auth/options";
-import { redirect, notFound } from "next/navigation";
-import { getClientById } from "@/lib/client/getClientById";
-
+import { redirect } from "next/navigation";
+import { notFound } from "next/navigation";
 
 export default async function Profile() {
     const session = await getServerSession(authOptions);
     console.log("Sesión:", session?.user?.fullName);
     console.log("Sesión:", session?.user?.token);
 
-    if (!session) redirect("/login");
-
+    if (!session) {
+        redirect("/login");
+    }
     const user = session?.user;
-    
-    console.log(user.clientId);
-    if(!user?.clientId) return console.log("No hay clientId en la sesión");
-
-    const clientProfile = await getClientById(Number(user.clientId), user.token);
-
-    if(!clientProfile) return console.log("No se encontró el perfil del cliente");
+    if(!user.clientId) return notFound();
 
     return (
         <div>
-            <Header user={clientProfile} />
+            <Header fullName={user?.fullName} token={user?.token}/>
             <PetsList clientId={user.clientId} token={user?.token} />
             <Appointments />
             <VeterinaryProducts />
