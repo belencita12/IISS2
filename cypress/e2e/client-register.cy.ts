a6f57360b6d22549642219026276d9eecd2d1e7d
makiYamaGin <144964474+makiYamaGin@users.noreply.github.com>
2025-04-07 10:39:48 -0300

    cambios en client-register

--- cypress/e2e/client-register.cy.ts
+++ cypress/e2e/client-register.cy.ts
@@ -33,40 +33,41 @@ describe('Registro de Cliente', () => {
         cy.get('input[placeholder="Ingrese el nombre del cliente"]').type('Juan');
         cy.get('input[placeholder="Ingrese el apellido del cliente"]').type('Pérez');
         cy.get('input[placeholder="ejemplo@gmail.com"]').type(uniqueEmail);
-        cy.get('input[placeholder="Ingrese la dirrección del cliente"]').type(randomAddress);
+        cy.get('input[placeholder="Ingrese la dirección del cliente"]').type(randomAddress);
         cy.get('input[placeholder="Ingrese el número de teléfono"]').type(randomPhoneNumber);
         cy.get('input[placeholder="Ingrese el RUC del cliente"]').type(randomRuc);
         cy.get('button').contains('Agregar cliente').click();
         cy.wait(2000);
-        cy.get('section[aria-label="Notifications alt+T"]')
-            .should('be.visible') 
-            .and('contain', 'Registro exitoso!');
+        cy.contains('Cliente registrado con éxito').should('be.visible');
+        cy.visit('/dashboard/clients');
+        cy.wait(5000);
     });
 
-    it('Debe mostrar error al registrar un usuario con el mismo email', () => {
-        const randomNumber = Math.floor(Math.random() * 100000);
-        uniqueEmail = `testuser${randomNumber}@gmail.com`;
-        const randomAddress = `Avda${randomNumber}`;
-        const randomPhoneNumber = `+595971${Math.floor(100000 + Math.random() * 900000)}`;
-        const randomRuc = `${Math.floor(1000000 + Math.random() * 900000)}-1`;
+    // it('Debe mostrar error al registrar un usuario con el mismo email', () => {
+    //     const randomNumber = Math.floor(Math.random() * 100000);
+    //     uniqueEmail = `testuser${randomNumber}@gmail.com`;
+    //     const randomAddress = `Avda${randomNumber}`;
+    //     const randomPhoneNumber = `+595971${Math.floor(100000 + Math.random() * 900000)}`;
+    //     const randomRuc = `${Math.floor(1000000 + Math.random() * 900000)}-1`;
     
-        cy.get('input[placeholder="Ingrese el nombre del cliente"]').type('Juan');
-        cy.get('input[placeholder="Ingrese el apellido del cliente"]').type('Pérez');
-        cy.get('input[placeholder="ejemplo@gmail.com"]').type(uniqueEmail);
-        cy.get('input[placeholder="Ingrese la dirrección del cliente"]').type(randomAddress);
-        cy.get('input[placeholder="Ingrese el número de teléfono"]').type(randomPhoneNumber);
-        cy.get('input[placeholder="Ingrese el RUC del cliente"]').type(randomRuc);
-        cy.get('button').contains('Agregar cliente').click();
-        cy.wait(2000);
-        cy.get('section[aria-label="Notifications alt+T"]')
-            .should('be.visible') 
-            .and('contain', 'Correo ya registrado');
-    });
+    //     cy.get('input[placeholder="Ingrese el nombre del cliente"]').type('Juan');
+    //     cy.get('input[placeholder="Ingrese el apellido del cliente"]').type('Pérez');
+    //     cy.get('input[placeholder="ejemplo@gmail.com"]').type(uniqueEmail);
+    //     cy.get('input[placeholder="Ingrese la dirección del cliente"]').type(randomAddress);
+    //     cy.get('input[placeholder="Ingrese el número de teléfono"]').type(randomPhoneNumber);
+    //     cy.get('input[placeholder="Ingrese el RUC del cliente"]').type(randomRuc);
+    //     cy.get('button').contains('Agregar cliente').click();
+    //     cy.wait(2000);
+    //     cy.get('section[aria-label="Notifications alt+T"]')
+    //         .should('be.visible') 
+    //         .and('contain', 'Correo ya registrado');
+    // });
 
 
     it('Evitar que se envie el formulario con campos vacios', () => {
         cy.get('button').contains('Agregar cliente').click();
         cy.contains('El nombre es obligatorio').should('be.visible');
+        cy.wait(5000);
     });
 
     it('Debe mostrar un mensaje de error si el email no es válido', () => {
@@ -76,5 +77,6 @@ describe('Registro de Cliente', () => {
         cy.get('button').contains('Agregar cliente').click();
         cy.contains('El formato del correo electrónico no es válido').should('be.visible');
         cy.contains("button", "Cancelar").click();
+        cy.wait(5000);
     });
 });
