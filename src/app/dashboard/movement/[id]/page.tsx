e30d453f6805bd3a4f557a29d31fcc0f86d96836
TuNombreDeUsuario <labelhu2014@gmail.com>
2025-04-16 09:59:14 -0300

    resuelto conflictos con el params

--- src/app/dashboard/movement/[id]/page.tsx
+++ src/app/dashboard/movement/[id]/page.tsx
@@ -3,21 +3,17 @@ import authOptions from "@/lib/auth/options";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 
-interface MovementDetailPageProps {
-  params: {
-    id: string;
-  };
-}
+export default async function MovementDetailPage({ params }: { params: Promise< {id: string }>}) {
 
-export default async function MovementDetailPage({ params }: MovementDetailPageProps) {
+  const resolvedParams = await params;
+  const id = Number(resolvedParams.id);
   const session = await getServerSession(authOptions);
+  const token = session?.user?.token || "";
+
 
   if (!session) {
     redirect("/login");
   }
-
-  const token = session.user.token;
-  const id = Number(params.id);
-
+  
   return <MovementDetails token={token} id={id} />;
 }
