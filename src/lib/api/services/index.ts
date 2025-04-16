6e8a541fbe3a46768745a86c88276307ab8d8ede
Bryan Quintana <bryan.quintana@fiuni.edu.py>
2025-04-16 15:19:46 -0300

    Remove provider service and related exports

--- src/lib/api/services/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './providerService';
-
-// A medida que se creen más servicios, se pueden exportar desde aquí
-// Por ejemplo:
-// export * from './petService';
-// export * from './userService';
-// etc.
