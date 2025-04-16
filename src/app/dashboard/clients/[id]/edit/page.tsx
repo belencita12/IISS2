989ec832f64d41b57e25b829cecb86d9f1a4113a
makiYamaGin <144964474+makiYamaGin@users.noreply.github.com>
2025-04-16 17:41:58 -0300

    task-336-Editar client
    cambiando params

--- src/app/dashboard/clients/[id]/edit/page.tsx
+++ src/app/dashboard/clients/[id]/edit/page.tsx
@@ -3,11 +3,13 @@ import { redirect } from "next/navigation";
 import authOptions from "@/lib/auth/options";
 import EditClientForm from "@/components/admin/client/EditClientForm";
 
-export default async function EditClientPage({
-    params,
-}: {
-    params: { id: string };
-}) {
+interface PageProps {
+    params: {
+        id: string;
+    };
+}
+
+export default async function EditClientPage({ params }: PageProps) {
     const session = await getServerSession(authOptions);
 
     if (!session) {
