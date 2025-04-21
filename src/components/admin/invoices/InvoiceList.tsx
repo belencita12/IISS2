1c72355a592017c3c50f19afbe42fa045e66b197
TuNombreDeUsuario <labelhu2014@gmail.com>
2025-04-21 19:23:28 -0300

    trae correctamente los datos en la lista, aun no tiene buen disenho

---
+++ src/components/admin/invoices/InvoiceList.tsx
@@ -0,0 +1,54 @@
+"use client";
+import { usePaginatedFetch } from "@/hooks/api/usePaginatedFetch";
+import { INVOICE_API } from "@/lib/urls";
+import { Invoice } from "@/lib/invoices/IInvoice";
+import { useEffect } from "react";
+import GenericTable, { Column } from "@/components/global/GenericTable";
+
+interface InvoiceListProps {
+  token: string;
+}
+
+const InvoiceList = ({ token }: InvoiceListProps) => {
+  const {
+    data: invoices,
+    loading,
+    error,
+    pagination,
+    setPage,
+  } = usePaginatedFetch<Invoice>(INVOICE_API, token, {
+    autoFetch: true,
+    initialPage: 1,
+    size: 7, // Cantidad por página
+  });
+
+  useEffect(() => {
+   
+  }, []);
+
+  const columns: Column<Invoice>[] = [
+    { header: "Nro Factura", accessor: "invoiceNumber" },
+    { header: "Cliente", accessor: "clientName" },
+    { header: "RUC", accessor: "ruc" },
+    { header: "Fecha", accessor: "issueDate" },
+    { header: "Tipo", accessor: (i) => (i.type === "CASH" ? "Contado" : "Crédito") },
+    { header: "Total", accessor: (i) => `${i.total.toLocaleString()} Gs.` },
+    { header: "Pagado", accessor: (i) => `${i.totalPayed.toLocaleString()} Gs.` },
+  ];
+  
+  if (loading) return <p>Cargando...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  return (
+    <GenericTable
+      data={invoices}
+      columns={columns}
+      pagination={pagination}
+      onPageChange={setPage}
+      isLoading={loading}
+      emptyMessage="No se encontraron facturas"
+    />
+  );
+}
+
+export default InvoiceList;
