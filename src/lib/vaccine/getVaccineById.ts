9b843855049d953efa6c5e361a99721bf0f9beeb
AnniaBenitez <anniamicaela@gmail.com>
2025-04-22 00:03:25 -0300

    cambios solicitados por bryan

--- src/lib/vaccine/getVaccineById.ts
+++ src/lib/vaccine/getVaccineById.ts
@@ -1,21 +1,21 @@
 // lib/vaccine/getVaccineById.ts
+import { VACCINE_API } from "../urls";
+
 export async function getVaccineById(token: string, id: number) {
-    const url = new URL(`${process.env.NEXT_PUBLIC_BASE_URL}/vaccine/${id}`);
-  
-    const response = await fetch(url.toString(), {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
-      },
-    });
-  
-    if (!response.ok) {
-      console.error("Error en la respuesta de la API:", response.status, response.statusText);
-      throw new Error("Error al obtener la vacuna");
-    }
-  
-    const data = await response.json();
-    //console.log("Respuesta de la API (getVaccineById):", data); 
-    return data;
+  const url = `${VACCINE_API}/${id}`;
+
+  const response = await fetch(url, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!response.ok) {
+    console.error("Error en la respuesta de la API:", response.status, response.statusText);
+    throw new Error("Error al obtener la vacuna");
   }
-  
+
+  const data = await response.json();
+  return data;
+}
