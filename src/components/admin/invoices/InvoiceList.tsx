9339b82a7f1c9d2707bdb126f4ed65e41e7d2302
TuNombreDeUsuario <labelhu2014@gmail.com>
2025-04-22 10:34:01 -0300

    filtro para buscar entre precios, tambien modifique un chiqui el invoicenumericfilter, para que no quede en 0 el input al borrar el numero

--- src/components/admin/invoices/InvoiceList.tsx
+++ src/components/admin/invoices/InvoiceList.tsx
@@ -1,20 +1,76 @@
 "use client";
+import { useState } from "react";
 import InvoiceTable from "./InvoiceTable";
 import InvoiceNumericFilter from "./filters/InvoiceNumericFilter";
+import { usePaginatedFetch } from "@/hooks/api/usePaginatedFetch";
+import { INVOICE_API } from "@/lib/urls";
+import { Invoice } from "@/lib/invoices/IInvoice";
+import { toast } from "@/lib/toast";
+import { GetInvoiceQueryParams } from "@/lib/invoices/IInvoice";
 
 interface InvoiceListProps {
   token: string;
 }
 
-
 const InvoiceList = ({ token }: InvoiceListProps) => {
+  const [filters, setFilters] = useState<GetInvoiceQueryParams>({
+    page: 1,
+    fromTotal: undefined,
+    toTotal: undefined,
+  });
+
+  const {
+    data,
+    loading: isLoading,
+    error,
+    pagination = { currentPage: 1, totalPages: 1, totalItems: 0, pageSize: 10 },
+    setPage,
+    search,
+  } = usePaginatedFetch<Invoice>(INVOICE_API, token, {
+    initialPage: 1,
+    size: 7,
+    autoFetch: true,
+    extraParams:{
+      fromTotal: filters?.fromTotal ? Number(filters?.fromTotal) : undefined,
+      toTotal: filters?.toTotal ? Number(filters?.toTotal) : undefined,
+    },
+  });
+
+  // Actualiza los resultados al cambiar los filtros
+  const handleFilterChange = (updatedFilters: GetInvoiceQueryParams) => {
+    const { page, size, ...safeFilters } = updatedFilters; 
+    setFilters((prev) => ({
+      ...prev,
+      ...safeFilters,
+      page: 1, 
+    }));
+    search(safeFilters as Record<string, unknown>);
+  };
+
+  if (error) toast("error", error.message || "Error al cargar las facturas");
+
   return (
     <div className="p-4 mx-auto">
-        
+       <div className="max-w-6xl mx-auto p-4 space-y-6">
+      <InvoiceNumericFilter filters={filters} setFilters={handleFilterChange} />
+      </div>
       <div className="flex justify-between items-center mb-4">
         <h2 className="text-3xl font-bold">Facturas</h2>
       </div>
-      <InvoiceTable token={token} />
+
+      <InvoiceTable
+        emptyMessage="No se encontraron facturas"
+        onPageChange={setPage}
+        token={token}
+        isLoading={isLoading}
+        data={data || []}
+        pagination={{
+          currentPage: pagination.currentPage,
+          totalPages: pagination.totalPages,
+          totalItems: pagination.totalItems,
+          pageSize: pagination.pageSize,
+        }}
+      />
     </div>
   );
 };
