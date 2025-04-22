c3f0abc00b1ba5412608c44353a95f0157a1fcd3
TuNombreDeUsuario <labelhu2014@gmail.com>
2025-04-22 18:59:46 -0300

    quito el toast al cargar un detalle de la factura

--- src/components/admin/invoices/InvoiceList.tsx
+++ src/components/admin/invoices/InvoiceList.tsx
@@ -39,7 +39,6 @@ const InvoiceList = ({ token }: InvoiceListProps) => {
     },
   });
 
-  // Actualiza los resultados al cambiar los filtros
   const handleFilterChange = (updatedFilters: GetInvoiceQueryParams) => {
     const { page, size, ...safeFilters } = updatedFilters; 
     setFilters((prev) => ({
@@ -55,8 +54,8 @@ const InvoiceList = ({ token }: InvoiceListProps) => {
   return (
     <div className="p-4 mx-auto">
        <div className="max-w-6xl mx-auto p-4 space-y-6">
-      <InvoiceNumericFilter filters={filters} setFilters={handleFilterChange} />
-      <InvoiceDateFilter filters={filters} setFilters={handleFilterChange} />
+        <InvoiceNumericFilter filters={filters} setFilters={handleFilterChange} />
+        <InvoiceDateFilter filters={filters} setFilters={handleFilterChange} />
       </div>
       <div className="flex justify-between items-center mb-4">
         <h2 className="text-3xl font-bold">Facturas</h2>
