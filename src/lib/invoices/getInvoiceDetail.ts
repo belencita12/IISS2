0012f4dd3c95cf0dc35f226d0aefef7b9325d39b
Agus3160 <ampharos321@hotmail.com>
2025-04-23 14:29:58 -0300

    invoice detail filter

--- src/lib/invoices/getInvoiceDetail.ts
+++ src/lib/invoices/getInvoiceDetail.ts
@@ -2,14 +2,14 @@ import { INVOICE_DETAIL_API } from "@/lib/urls";
 import { InvoiceDetailResponse } from "./IInvoice";
 
 export async function getInvoiceDetail(
-  invoiceNumber: string, 
+  invoiceId: string, 
   token: string
 ): Promise<InvoiceDetailResponse> {
   try {
     const url = new URL(`${INVOICE_DETAIL_API}`);
     url.searchParams.append("page", "1");
     url.searchParams.append("size", "50");
-    url.searchParams.append("invoiceNumber", invoiceNumber);
+    url.searchParams.append("invoiceId", invoiceId);
 
     const response = await fetch(url.toString(), {
       method: "GET",
