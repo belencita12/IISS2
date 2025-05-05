9cd5d61e67c93aed4c03b4f7cba9bb316829b3ea
NicolasCabrera7 <andres.nicolas.cabrera@gmail.com>
2025-05-05 20:31:17 -0300

    Pantalla mejorada de detalle clientes(Admin)

--- src/app/dashboard/clients/[id]/page.tsx
+++ src/app/dashboard/clients/[id]/page.tsx
@@ -7,6 +7,7 @@ import { IUserProfile } from "@/lib/client/IUserProfile";
 import PaginatedPetsTable from "@/components/admin/pet/PaginatedPetsTable";
 import { notFound } from "next/navigation";
 import Link from "next/link";
+import ClientAppointmentList from "@/components/admin/appointment/AppointmentListByClient"; // ✅ asegúrate de que el path sea correcto
 
 export default async function ClientDetails(
     { params }: { params: Promise<{ id: string }> }
@@ -19,21 +20,28 @@ export default async function ClientDetails(
     
     const client = await getClientById(clientId, token) as IUserProfile;
     if (!client) {
-        //console.error(`Cliente con id ${id} no encontrado`);
         return notFound();
-      }
-    
+    }
+
     return (
         <>
             <ClientProfileSection {...client} />
-            <section className="mx-auto px-1 md:px-24">
+
+            <section className="mx-auto mt-10 px-1 md:px-24">
                 <div className="flex justify-between items-center mb-4">
                     <h2 className="text-xl">Mascotas</h2>
                     <Link href={`/dashboard/clients/${id}/pet/register`}>
-                        <Button variant={"outline"} className="border-black border-solid">Agregar</Button>
+                        <Button variant="outline" className="border-black border-solid">
+                            Agregar
+                        </Button>
                     </Link>
                 </div>
-                    <PaginatedPetsTable token={token} id={clientId}/>
+                <PaginatedPetsTable token={token} id={clientId} />
+            </section>
+
+            <section className="mx-auto px-1 md:px-24 mt-10">
+                <h2 className="text-xl font-semibold mb-4">Citas del Cliente</h2>
+                <ClientAppointmentList token={token} clientRuc={client.ruc} />
             </section>
         </>
     );
