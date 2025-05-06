159ff9559567a98dbc1b33b26925e2979c3c915b
Matias Brunaga <genaro.brunaga@fiuni.edu.py>
2025-05-06 15:40:59 -0300

    correccion de inconsistencias y errores de compilacion

--- src/components/profile/Appointments.tsx
+++ src/components/profile/Appointments.tsx
@@ -56,7 +56,7 @@ export const Appointments = ({ token, ruc }: AppointmentsProps) => {
     switch (st) {
       case "COMPLETED":
         return { txt: "Completada", style: "bg-green-100 text-green-800" };
-      case "CANCELED":
+      case "CANCELLED":
         return { txt: "Cancelada", style: "bg-red-100 text-red-800" };
       case "IN_PROGRESS":
         return { txt: "En progreso", style: "bg-blue-100 text-blue-800" };
@@ -166,58 +166,6 @@ export const Appointments = ({ token, ruc }: AppointmentsProps) => {
           </Link>
         </div>
       </div>
-
-export const Appointments = () => {
-    const appointments = [
-        {
-            id: 1,
-            title: "Control Veterinario",
-            date: "23/09/2022",
-            time: "9:00 AM",
-            icon: <CalendarIcon className="w-10 h-10 text-gray-700" />,
-        },
-        {
-            id: 2,
-            title: "Vacunación",
-            date: "15/10/2022",
-            time: "11:0 AM",
-            icon:  <Syringe className="w-10 h-10 text-gray-700" />,
-        },
-    ];
-
-    return (
-        <section className="max-w-5xl mx-auto mt-10 p-6 bg-white">
-            <div className="flex justify-between items-center">
-                <h2 className="text-2xl font-bold">Citas Agendadas</h2>
-
-                <Button asChild className="mt-2 flex items-center gap-2">
-                    <Link href="/">
-                        <CalendarIcon className="w-5 h-5" />
-                        Agendar una cita
-                    </Link>
-                </Button>
-
-            </div>
-
-            <div className="mt-6">
-                {appointments.map((appointment) => (
-                    <div key={appointment.id} className="flex justify-between items-center py-4 border-b">
-                        <div className="flex items-center gap-4">
-                        {appointment.icon}
-                            <div>
-                                <p className="font-semibold">{appointment.title}</p>
-                                <p className="text-gray-500 text-sm">{appointment.date}</p>
-                            </div>
-                        </div>
-                        <p className="font-medium flex items-center gap-2">
-                            <ClockIcon className="w-5 h-5" />
-                            {appointment.time}
-                        </p>
-                    </div>
-                ))}
-            </div>
-        </section>
-    );
       <div className="mt-10">
         {loading ? (
           <AppointmentsTableSkeleton />
