3c12bac837eb8eca2081ef6790b35684731e4909
Gessica <adarogessica@gmail.com>
2025-05-12 07:43:01 -0300

    pantalla inicio ya es accesible con sesion activa/inactiva

---
+++ src/app/home/page.tsx
@@ -0,0 +1,21 @@
+import Hero from "@/components/unauthorizePage/Hero";
+import Products from "@/components/unauthorizePage/Products";
+import Services from "@/components/unauthorizePage/Services";
+import Image from "next/image";
+
+export default async function HomePublic() {
+  return (
+    <div className="sm:px-[6.5%] px-5 max-w-full overflow-hidden">
+      <Hero />
+      <Services />
+      <Products />
+      <Image
+        src="/image gato.png"
+        alt="Gato"
+        width={1500}
+        height={400}
+        className="object-contain rounded-xl w-full max-w-[1500px] mx-auto"
+      />
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,13 @@
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import authOptions from "@/lib/auth/options";
-import Hero from "@/components/unauthorizePage/Hero";
-import Products from "@/components/unauthorizePage/Products";
-import Services from "@/components/unauthorizePage/Services";
-import Image from "next/image";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
   if (session?.user?.token) {
-    if (session.user.roles.includes("ADMIN")) {
-      redirect("/dashboard");
-    } else {
-      redirect("/user-profile");
-    }
+    return redirect(
+      session.user.roles.includes("ADMIN") ? "/dashboard" : "/user-profile"
+    );
   }
-  return (
-    <div className="sm:px-[6.5%] px-5 max-w-full overflow-hidden">
-      <Hero />
-      <Services />
-      <Products />
-      <Image
-        src="/image gato.png"
-        alt="Gato"
-        width={1500}
-        height={400}
-        className="object-contain rounded-xl w-full max-w-[1500px] mx-auto"
-      />
-    </div>
-  );
+  return redirect("/home");
 }
