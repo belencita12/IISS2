39f61c1be1695d4cf8564e7e65d59d617cba241f
SAZAUNI <santiago.zarza@fiuni.edu.py>
2025-05-13 02:50:51 -0300

    Modificado: uso de las variables de tailwind para los colores

--- src/components/global/Footer.tsx
+++ src/components/global/Footer.tsx
@@ -2,12 +2,12 @@ export default function Footer() {
   return (
     <footer className="relative mt-10 bg-gray-100">
 
-      <div className="absolute top-0 left-0 right-0 h-px bg-gradient-to-r from-transparent via-[#a855f7]/30 to-transparent" />
+      <div className="absolute top-0 left-0 right-0 h-px bg-gradient-to-r from-transparent via-myPurple-primary/30 to-transparent" />
       
       <div className="container mx-auto px-4 py-8">
         <div className="flex flex-col items-center justify-center space-y-4">
           <div className="group cursor-default">
-            <h3 className="font-semibold text-lg bg-gradient-to-r from-[#a855f7] to-[#ec4899] bg-clip-text text-transparent ">
+            <h3 className="font-semibold text-lg bg-gradient-to-r from-myPurple-primary to-myPink-primary bg-clip-text text-transparent">
               Sistema de Veterinaria
             </h3>
           </div>
@@ -17,7 +17,7 @@ export default function Footer() {
             <span className="sm:block text-gray-600">|</span>
             <a 
               href="/terms"
-              className="text-[#a855f7] hover:text-[#ec4899] transition-colors duration-300 font-bold"
+              className="text-myPurple-primary hover:text-myPink-primary transition-colors duration-300 font-bold"
             >
               Términos & Condiciones
             </a>
