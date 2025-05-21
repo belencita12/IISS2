3271c6e0841d547c64bfc67062424b10c7c07bef
MatiasNish <genaro.brunaga@fiuni.edu.py>
2025-05-21 17:35:12 -0300

    test de reportes y restauracion de elementos

---
+++ cypress/e2e/appointment-report.cy.ts
@@ -0,0 +1,31 @@
+describe('Reporte de Citas - Admin', () => {
+    const SESSION_KEY = "sessionToken";
+    const USER = {
+        email: Cypress.env("USER_EMAIL_A"),
+        password: Cypress.env("USER_PASSWORD_A")
+    };
+
+    beforeEach(() => {
+        cy.clearCookies();
+        cy.clearLocalStorage();
+        cy.session(SESSION_KEY, () => {
+            cy.loginAndSetSession(SESSION_KEY, USER.email, USER.password);
+            cy.wait(5000);
+            cy.url().should('include', '/dashboard');
+        });
+        cy.visit('/dashboard/appointment');
+        cy.url().should('include', '/dashboard/appointment');
+        cy.wait(2000);
+    });
+
+    it('debe mostrar un toast de error si no se selecciona el rango de fechas y se intenta exportar', () => {
+        cy.get('button').contains('Exportar').click();
+    });
+
+    it('debe generar un PDF correctamente al elegir un rango de fechas válido', () => {
+        cy.get('#startDate').type('2024-05-01');
+        cy.get('#endDate').type('2024-05-20');
+        cy.wait(6000);
+        cy.get('button').contains('Exportar').click();
+    });
+});
