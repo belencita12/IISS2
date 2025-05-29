d17a90ba7e4d15944d1e027991bdd5620d805f60
LourdesVal <lourdesvalenzuela@fiuni.edu.py>
2025-05-29 19:23:23 -0300

    usar todo el ancho de la pantalla en lista de depositos

--- src/components/deposit/DepositDetails.tsx
+++ src/components/deposit/DepositDetails.tsx
@@ -117,7 +117,7 @@ export default function DepositDetails({ token, stockId }: DepositDetailsProps) 
   };
 
   return (
-    <div className="w-full mx-auto p-4">
+    <div className="max-w-7xl mx-auto p-4">
       <div className="mb-2 mt-2">
         <Button
           variant="outline"
