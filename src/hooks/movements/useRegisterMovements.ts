a413cd7589294360c11ee7efc84b28de0a664523
LourdesVal <lourdesvalenzuela@fiuni.edu.py>
2025-05-31 22:02:56 -0300

    mensaje si no hay suficiente cantidad de producto

--- src/hooks/movements/useRegisterMovements.ts
+++ src/hooks/movements/useRegisterMovements.ts
@@ -99,10 +99,18 @@ export const useRegisterMovement = (token: string) => {
       reset();
       return true;
     } catch (error: unknown) {
-      toast(
-        "error",
-        error instanceof Error ? error.message : "Error al registrar movimiento"
-      );
+      const errorMessage =
+        error instanceof Error ? error.message : "Error al registrar movimiento";
+
+      if (
+        typeof errorMessage === "string" &&
+        errorMessage.includes("Stock insuficiente")
+      ) {
+        toast("error", "No hay suficiente cantidad en el depósito.");
+      } else {
+        toast("error", errorMessage);
+      }
+
       return false;
     }
   };
