9b2c50772791aebe5d8119d7be22b32e5469ea76
TuNombreDeUsuario <labelhu2014@gmail.com>
2025-06-06 00:28:44 -0300

    feat: update filter button text to use translation for consistency across components

--- src/components/admin/movements/MovementList.tsx
+++ src/components/admin/movements/MovementList.tsx
@@ -69,7 +69,7 @@ export default function MovementListPage({ token }: Props) {
           className="text-sm h-8 px-2 text-gray-600 mr-[10px]"
           disabled={isFiltering}
           >
-          Limpiar filtros
+          {t("filters.clearFilters")}
           </Button>
         </div>
       )}
--- src/components/admin/product/ProductList.tsx
+++ src/components/admin/product/ProductList.tsx
@@ -168,7 +168,7 @@ export default function ProductListPage({ token }: ProductListProps) {
               className="text-sm h-8 px-2 text-gray-600 mr-[10px]"
               disabled={isFiltering}
               >
-              Limpiar filtros
+              {t("filters.clearFilters")}
               </Button>
           </div>
       )}
